1260f1612b89fa4148e214ea73c7d1f56c8d1613
堀田達也 <hottatatsuya@hottatatsuyanoMacBook-Pro.local>
2021-04-16 01:39:21 +0900

    router baser

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    base: '/Pokemon-List-By-Color/'
+  } ,
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'pokemon-from-color',
